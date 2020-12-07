18d247135b916a8243b3e9e3a9fe760bd24d6827
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2020-12-07 15:22:54 -0700

    Include a basic layout for the main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>TOOLBAR</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>SIDE DRAWER</Paper>
+        </Grid>
+        <Grid item xs={9}>
+          <Paper className={classes.paper}>MAIN</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>FOOTER</Paper>
+        </Grid>        
+      </Grid>
     </div>
   );
 }
