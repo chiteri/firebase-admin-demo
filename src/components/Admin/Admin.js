9004bee947d16b651718dc0f4fb7e6d9e5f65aba
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2020-12-09 16:28:20 -0700

    Use a higher-order component because the component is now class-based

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../hoc/Auxilliary';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
     root: {
       flexGrow: 1,
     },
@@ -13,28 +14,34 @@ const useStyles = makeStyles((theme) => ({
       textAlign: 'center',
       color: theme.palette.text.secondary,
     },
-  }));
+});
 
-function Admin() {
-    const classes = useStyles();
+class Admin extends Component {
+  render() {
+    const { classes } = this.props;
     return (
         <Aux>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-            <Paper className={classes.paper}>TOOLBAR</Paper>
+            <Paper className={this.state.classes.paper}>TOOLBAR</Paper>
             </Grid>
             <Grid item xs={3} >
-            <Paper className={classes.paper}>SIDE MENU</Paper>
+            <Paper className={this.state.classes.paper}>SIDE MENU</Paper>
             </Grid>
             <Grid item xs={9} >
-            <Paper className={classes.paper}>MAIN</Paper>
+            <Paper className={this.state.classes.paper}>MAIN</Paper>
             </Grid>
             <Grid item xs={12}>
-            <Paper className={classes.paper}>FOOTER</Paper>
+            <Paper className={this.state.classes.paper}>FOOTER</Paper>
             </Grid>        
           </Grid>
         </Aux>
     );
+  }
 }
 
-export default Admin;
+Admin.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Admin);
