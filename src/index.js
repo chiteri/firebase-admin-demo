6b833285361cac3c607310a3855742225efe40ef
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2020-12-10 17:47:26 -0700

    Include interceptors to execute code globally in app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+// An interceptor to handle requests
+axios.interceptors.request.use(request => {
+  console.log(request); // Do something with the request configurations 
+  return request;
+}, error => {
+  // Handle any errors
+  console.log(error);
+
+  // Make sure to forward the error to our request as we load it in a local component for handling
+  return Promise.reject(error); // Maybe show something globally on the UI or write to log file
+});
+
+// An interceptor to handle responses 
+axios.interceptors.response.use(response => {
+  console.log(response); // Do something with the response configurations 
+  return response;
+}, error => {
+  // Handle any errors
+  console.log(error);
+
+  // Make sure to forward the error to our response as we load it in a local component for handling
+  return Promise.reject(error); // Maybe show something globally on the UI or write to log file
+});
 
 ReactDOM.render(
   <React.StrictMode>
